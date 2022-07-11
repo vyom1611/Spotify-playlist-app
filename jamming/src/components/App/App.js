86270e6d204d70a4017ded209ce0d2a2066ae4eb
vyom <sharm843@umn.edu>
2022-07-11 14:19:05 +0530

    Passing Playlist to Tracklist

--- jamming/src/components/App/App.js
+++ jamming/src/components/App/App.js
@@ -6,8 +6,11 @@ import SearchResults from '../SearchResults/SearchResults'
 
 function App(props) {
 
-  const [searchResults, setSearchResults] = useState([{ artist:"The Smile", name: "Pana-vision", album: "crisis", id: 1},{ artist:"Justin Bieber", name: "Ghost", album: "Justice", id: 2}])
-    
+  const [searchResults, setSearchResults] = useState([{ artist:"The Smile", name: "Pana-vision", album: "-", id: 1},{ artist:"Justin Bieber", name: "Ghost", album: "Justice", id: 2}])
+  
+  const [playlistName, setPlaylistName] = useState("Peaches");
+  const [playlistTracks, setPlaylistTracks] = useState({name: "Peaches", artist: "Justin Bieber", album: "Justice", id:5});
+
     return (
     <div>
       <h1>Ja<span class="highlight">mmm</span>ing</h1>
@@ -15,7 +18,7 @@ function App(props) {
         <SearchBar />
         <div class="App-playlist">
           <SearchResults results={searchResults}/>
-          <Playlist />
+          <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
         </div>
       </div>
     </div>
--- jamming/src/components/Playlist/Playlist.js
+++ jamming/src/components/Playlist/Playlist.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Playlist.css'
+import TrackList from '../TrackList/TrackList'
 
-function Playlist() {
+function Playlist(props) {
   return (
     <div className="Playlist">
         <input value="New Playlist"/>
-        {/* <!-- Add a TrackList component --> */}
+        <TrackList tracks={props.playlistTracks.playlistTracks} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
   )

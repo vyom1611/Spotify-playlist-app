bc5395288d699b3750763cc8af05d705d22a3e86
vyom <sharm843@umn.edu>
2022-07-16 22:00:09 +0530

    Adding Spotify Web API access token validation

---
+++ jamming/src/util/Spotify.js
@@ -0,0 +1,31 @@
+const clientID = "67a416a44c364fdf82a1bcbfe7eae165";
+const redirectUri = "http://localhost:3000"
+
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken
+        } 
+
+        //check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            //Grabbing new access token after clearing the parameters
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+
+            return accessToken
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl
+        }
+    }
+}
+
+export default Spotify;

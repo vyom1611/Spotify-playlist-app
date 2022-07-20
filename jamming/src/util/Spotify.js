698a3c15dee467c0d5d40d951d59687e95b98b90
vyom <sharm843@umn.edu>
2022-07-20 15:47:18 +0530

    Added Spotify search request from API endpoint

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -1,4 +1,6 @@
-const clientID = "67a416a44c364fdf82a1bcbfe7eae165";
+import 'dotenv/config';
+
+const clientID = process.env.CLIENT_ID;
 const redirectUri = "http://localhost:3000"
 
 let accessToken;
@@ -25,7 +27,33 @@ const Spotify = {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl
         }
+    },
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        //making GET method from spotify API access token endpoint
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+        { headers: {
+            Authorization: `Bearer ${accessToken}`
+        }})
+        //Converting response to json
+        .then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artist[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        })
+
     }
+
 }
 
 export default Spotify;
